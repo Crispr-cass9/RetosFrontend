f28ee80b591c692f1fc2d9526af62c69f3ea7386
Crispr-cass9 <Cristian.jovani2001@gmail.com>
2024-04-28 17:26:20 +0200

    Finalizado el diseño de pc

--- expenses-chart-component-main/script.js
+++ expenses-chart-component-main/script.js
@@ -1,5 +1,25 @@
 let barras = document.querySelectorAll(".bar")
 
+
+function buscar_mayor(lista){
+
+  gastos = []
+  
+  //Obtenemos los gastos diarios y los almacenamos en una lista
+  lista.forEach(day => {
+    gastos.push(day.amount);
+  });
+  
+  mayor = Math.max(...gastos)
+  
+  //Mediante  una regla de 3 calculamos el porcentaje de alto de cada barra
+  lista.forEach((day, index) => {
+    porcentaje = day.amount * 100 / mayor
+    barras[index].style.height = porcentaje+"%"
+  });
+
+}
+
 function solicitud(){
 
   axios.get('./data.json')
@@ -12,22 +32,7 @@ function solicitud(){
   });
 
 }
-// mayor --> 100%
-// valor -->  X
 
-function buscar_mayor(lista){
-  gastos = []
-  lista.forEach(day => {
-    gastos.push(day.amount);
-  });
-  mayor = Math.max(...gastos)
-  
-  lista.forEach((day, index) => {
-    porcentaje = day.amount * 100 / mayor
-    console.log("{$porcentaje}%");
-    barras[index].style.height = porcentaje+"%"
-  });
 
-}
 
 solicitud()
