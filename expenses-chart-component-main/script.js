d421face7d3a362d97219a103350fed780d94f26
Crispr-cass9 <Cristian.jovani2001@gmail.com>
2024-04-29 13:31:00 +0200

    Finalizado estilo y funcionalidad de día de semana y toast

--- expenses-chart-component-main/script.js
+++ expenses-chart-component-main/script.js
@@ -1,5 +1,16 @@
 let barras = document.querySelectorAll(".bar")
+let fecha = new Date();
+fecha = fecha.getUTCDay();
+let toasts = document.querySelectorAll(".toast")
+if (fecha == 0){
+  fecha = 7
+}
 
+
+
+function obtener_dia(numero_dia){
+  barras[numero_dia-1].style.backgroundColor="hsl(186, 34%, 60%)"
+}
 
 function buscar_mayor(lista){
 
@@ -14,8 +25,9 @@ function buscar_mayor(lista){
   
   //Mediante  una regla de 3 calculamos el porcentaje de alto de cada barra
   lista.forEach((day, index) => {
-    porcentaje = day.amount * 100 / mayor
+    porcentaje = day.amount * 70 / mayor
     barras[index].style.height = porcentaje+"%"
+    toasts[index].textContent = "$ " + day.amount
   });
 
 }
@@ -25,14 +37,30 @@ function solicitud(){
   axios.get('./data.json')
   .then(response => {
     console.log(response.data); 
-    buscar_mayor(response.data)
+    buscar_mayor(response.data);
+    obtener_dia(fecha);
   })
   .catch(error => {
     console.error('Error al obtener los datos:', error);
   });
 
 }
-
-
-
 solicitud()
+
+barras.forEach((barra, index) => {
+
+  barra.addEventListener("mouseover", (e) =>{
+    e.target.style.filter = "brightness(1.3)"
+    console.log(index);
+    toasts[index].style.display = "flex"
+
+  })
+
+  barra.addEventListener("mouseout", (e) =>{
+    e.target.style.filter = "brightness(1)"
+    toasts[index].style.display = "none"
+
+
+  })
+
+});
